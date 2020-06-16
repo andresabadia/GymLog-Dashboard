57cf41b005c6a7201a5a749d908a828b794de033
Andres Abadia <andres.abadia@id-ex.de>
2020-06-16 22:29:43 +0200

    payment added to the navigation for unity fitness

--- src/store.js
+++ src/store.js
@@ -13,7 +13,8 @@ export default new Vuex.Store({
       gym_owner: 'Owner',
       date: '2019-04-25 21:59:12.718',
       gym_id: ''
-    }
+    },
+    isUnityFitness: false
   },
   mutations: {
     showLoading: (state, payload) => {
@@ -21,12 +22,20 @@ export default new Vuex.Store({
     },
     checkUserId: (state, payload) => {
       // console.log(localStorage.getItem('gl-user'),sessionStorage.getItem('gl-user'))
-      if (localStorage.getItem('gl-user') != null || sessionStorage.getItem('gl-user') != null) {
+      if (
+        localStorage.getItem('gl-user') != null ||
+        sessionStorage.getItem('gl-user') != null
+      ) {
         state.userId = true
         if (localStorage.getItem('gl-user') != null) {
           state.glUser = JSON.parse(localStorage.getItem('gl-user'))
         } else {
           state.glUser = JSON.parse(sessionStorage.getItem('gl-user'))
+        }
+        if (state.glUser.gym_id === 'uf') {
+          state.isUnityFitness = true
+        } else {
+          state.isUnityFitness = false
         }
         // console.log('user id set to true')
       } else {
@@ -48,9 +57,10 @@ export default new Vuex.Store({
   },
   actions: {
     asyncSetDate: ({ commit, state }, payload) => {
-      axios.post('php/last_update.php', {
-        'gym_id': state.glUser.gym_id
-      })
+      axios
+        .post('php/last_update.php', {
+          gym_id: state.glUser.gym_id
+        })
         .then(res => {
           commit('setDate', res.data[0].date)
         })
